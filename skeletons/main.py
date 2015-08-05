2fb8a60d185e7ea290da5eab665be66e886c0260
Ant <anthony@dervishsoftware.com>
2015-08-05 21:04:39 +0100

    Update python skeleton

--- skeletons/main.py
+++ skeletons/main.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python
+"""
+A test program
+"""
 from __future__ import division, absolute_import, print_function
-__author__ = 'Anthony Dervish'
+__author__ = '<+AUTHOR+>'
 
 import logging
-import logging.config
 import argparse
 
-def configure_logging(log_level='DEBUG'):
-    stream_handler = (lambda x: 'logging.StreamHandler' if x else 'logging.NullHandler')(log_level)
-    handlers_dict = {'console': {'class': stream_handler, 'level': log_level}}
-    LOG_CONFIG = {'version': 1, 'handlers': handlers_dict, 'root': {'handlers': ['console'], 'level': logging.DEBUG} }
-    logging.config.dictConfig(LOG_CONFIG)
-
-""" ----------------------------------------------------------------------
-MAIN
----------------------------------------------------------------------- """
 
 def main():
-    parser = argparse.ArgumentParser(description='Process some integers.')
+    """ ----------------------------------------------------------------------
+    MAIN
+    ---------------------------------------------------------------------- """
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--loglevel', default=None, help='Set the log level')
-    parser.add_argument('--dostuff', action='store_true', help='do stuff')
-
     args = parser.parse_args()
 
-    log_level = (lambda x: x if x else 'INFO')(args.loglevel)
-    configure_logging(log_level)
+    #{{{ Configure logging
+    LOG_FORMAT = "%(levelname)s %(asctime)s %(filename)s:%(lineno)d | %(message)s"
+    log_level = args.loglevel if args.loglevel else 'INFO'
+    logging.basicConfig(format=LOG_FORMAT, level=log_level)
+    #}}}
 
     logging.info("Starting!")
- 
+
+
 if __name__ == "__main__":
     main()
+
+# vi:foldmethod=marker:
+
